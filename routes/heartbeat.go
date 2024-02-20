071252455a673afa1f5dbe9e1d92e1b3f4d8ae7c
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-20 23:47:36 +1300

    Add a route for heartbeat

---
+++ routes/heartbeat.go
@@ -0,0 +1,9 @@
+package routes
+
+import "net/http"
+
+func Heartbeat(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("."))
+}
