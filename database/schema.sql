011f28ed429ce063f04eca470e66f5a1134441ab
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-26 01:11:38 +1300

    Update schemas to only create tables if not existing

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     uuid text PRIMARY KEY,
     username text NOT NULL UNIQUE,
     FOREIGN KEY (uuid) REFERENCES authenticationData(uuid)
 );
 
-CREATE TABLE authenticationData (
+CREATE TABLE IF NOT EXISTS authenticationData (
     uuid text PRIMARY KEY,
     hashed_password text NOT NULL,
     salt text NOT NULL
