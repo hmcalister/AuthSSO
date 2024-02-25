b19ebc0c8a2c5b1ecc004207171105281f9e8135
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-26 01:11:27 +1300

    Fix schema with semicolons

--- database/schema.sql
+++ database/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE users (
     uuid text PRIMARY KEY,
     username text NOT NULL UNIQUE,
     FOREIGN KEY (uuid) REFERENCES authenticationData(uuid)
-)
+);
 
 CREATE TABLE authenticationData (
     uuid text PRIMARY KEY,
     hashed_password text NOT NULL,
     salt text NOT NULL
-)
+);
