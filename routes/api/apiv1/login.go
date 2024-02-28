2b6da71fcb17573f2576ffa08a0cfe42402c8118
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-29 00:21:29 +1300

    Update login to use new userJsonData struct and application/json header format

--- routes/api/apiv1/login.go
+++ routes/api/apiv1/login.go
@@ -2,6 +2,7 @@ package apiv1
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -10,40 +11,39 @@ import (
 )
 
 func (api *ApiHandler) login(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	var user userJsonData
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		log.Error().Err(err).Msg("Found error parsing form during login")
+		log.Error().Err(err).Msg("Found error parsing request during login")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Could not parse form!"))
 		return
 	}
 
-	username := r.Form.Get("username")
-	if username == "" {
+	if user.Username == "" {
 		log.Info().Msg("Request did not include 'username' field!")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Request must include 'username' field!"))
 		return
 	}
 
-	password := r.Form.Get("password")
-	if password == "" {
+	if user.Password == "" {
 		log.Info().Msg("Request did not include 'password' field!")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Request must include 'password' field!"))
 		return
 	}
-	if len(password) > passwordMaxLen {
-		log.Info().Int("PasswordLen", len(password)).Msg("Password is too long! ")
+	if len(user.Password) > passwordMaxLen {
+		log.Info().Int("PasswordLen", len(user.Password)).Msg("Password is too long! ")
 		w.WriteHeader(http.StatusRequestEntityTooLarge)
 		w.Write([]byte(fmt.Sprintf("Password must be less than %v characters long!", passwordMaxLen)))
 		return
 	}
 
 	ctx := context.Background()
-	ok, err := api.databaseConnection.ValidateAuthenticationAttempt(ctx, username, password)
+	ok, err := api.databaseConnection.ValidateAuthenticationAttempt(ctx, user.Username, user.Password)
 	if err == database.ErrOnFetchUserDoesNotExist {
-		log.Info().Str("Username", username).Msg("Login Request for Invalid Username")
+		log.Info().Str("Username", user.Username).Msg("Login Request for Invalid Username")
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("No user with given username exists!"))
 		return
@@ -57,7 +57,7 @@ func (api *ApiHandler) login(w http.ResponseWriter, r *http.Request) {
 
 	// Actually check if the user is who they say they are
 	if !ok {
-		log.Info().Str("Username", username).Msg("Invalid login attempt!")
+		log.Info().Str("Username", user.Username).Msg("Invalid login attempt!")
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Invalid Username or Password."))
 		return
@@ -65,9 +65,9 @@ func (api *ApiHandler) login(w http.ResponseWriter, r *http.Request) {
 
 	// Now we can go about giving the JWT to authenticate in the future
 
-	token, err := api.generateJWT(username)
+	token, err := api.generateJWT(user.Username)
 	if err != nil {
-		log.Error().Err(err).Str("Username", username).Msg("Error during creation of JWT!")
+		log.Error().Err(err).Str("Username", user.Username).Msg("Error during creation of JWT!")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("An error occurred during authentication attempt, please try again"))
 		return
