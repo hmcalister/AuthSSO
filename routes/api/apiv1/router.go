3c72a4e2b2a231dff0416ee344f51970f3c73e6e
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-28 23:54:37 +1300

    Update struct creation with new fields

--- routes/api/apiv1/router.go
+++ routes/api/apiv1/router.go
@@ -21,9 +21,14 @@ func (api *ApiHandler) Router() *chi.Mux {
 	return api.apiRouter
 }
 
-func NewApiRouter() *ApiHandler {
+func NewApiRouter(db *database.DatabaseManager, tokenSecretKey []byte) *ApiHandler {
+	tokenAuth := jwtauth.New(tokenSigningMethod.Alg(), tokenSecretKey, nil)
+
 	apiRouterData := &ApiHandler{
-		apiRouter: chi.NewRouter(),
+		apiRouter:          chi.NewRouter(),
+		databaseConnection: db,
+		tokenSecretKey:     tokenSecretKey,
+		tokenAuth:          tokenAuth,
 	}
 
 	apiRouterData.apiRouter.Get("/heartbeat", heartbeat)
