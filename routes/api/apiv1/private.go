875cefdfb4daaaae0a858331d1b1f866328e9f54
Hayden McAlister <haydenmcalister49@gmail.com>
2024-02-28 23:55:10 +1300

    Add a simple private route test

---
+++ routes/api/apiv1/private.go
@@ -0,0 +1,21 @@
+package apiv1
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/rs/zerolog/log"
+)
+
+func private(w http.ResponseWriter, r *http.Request) {
+	_, claims, err := jwtauth.FromContext(r.Context())
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to parse JWT!")
+		w.Write([]byte("An error occurred during authentication. Please try again."))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Private Route, Welcome User %v\n", claims["Subject"])))
+}
