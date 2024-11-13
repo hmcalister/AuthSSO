f6cdf05e4d412c7a1ce7348ba154ab425a223b4c
Hayden McAlister <haydenmcalister49@gmail.com>
2024-11-13 22:29:43 +1300

    Update login.go to use Slog over Zerolog

--- authenticationMaster/login.go
+++ authenticationMaster/login.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/hmcalister/AuthSSO/database"
-	"github.com/rs/zerolog/log"
 )
 
 func (authMaster *AuthenticationMaster) Login(w http.ResponseWriter, r *http.Request) {
 	var requestCredentials httpRequestCredentials
 	err := json.NewDecoder(r.Body).Decode(&requestCredentials)
 	if err != nil {
-		log.Error().Err(err).Msg("Found error parsing request during login")
+		slog.Error("Found error parsing request during login", "Error", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Could not parse form!"))
 		return
 	}
 
 	if requestCredentials.Username == "" {
-		log.Info().Msg("Request did not include 'username' field!")
+		slog.Info("Request did not include 'username' field!")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Request must include 'username' field!"))
 		return
@@ -29,20 +29,20 @@ func (authMaster *AuthenticationMaster) Login(w http.ResponseWriter, r *http.Req
 
 	sanitizedUsername := authMaster.htmlSanitizer.Sanitize(requestCredentials.Username)
 	if requestCredentials.Username != sanitizedUsername {
-		log.Info().Msg("Username must not require sanitization!")
+		slog.Info("Username must not require sanitization!")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Username must not require sanitization!"))
 		return
 	}
 
 	if requestCredentials.Password == "" {
-		log.Info().Msg("Request did not include 'password' field!")
+		slog.Info("Request did not include 'password' field!")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Request must include 'password' field!"))
 		return
 	}
 	if len(requestCredentials.Password) > passwordMaxLen {
-		log.Info().Int("PasswordLen", len(requestCredentials.Password)).Msg("Password is too long! ")
+		slog.Info("Password is too long!", "PasswordLength", len(requestCredentials.Password))
 		w.WriteHeader(http.StatusRequestEntityTooLarge)
 		w.Write([]byte(fmt.Sprintf("Password must be less than %v characters long!", passwordMaxLen)))
 		return
@@ -53,19 +53,19 @@ func (authMaster *AuthenticationMaster) Login(w http.ResponseWriter, r *http.Req
 
 	ok, err := authMaster.databaseConnection.ValidateLoginAttempt(databaseQueryContext, requestCredentials.Username, requestCredentials.Password)
 	if databaseQueryContext.Err() == context.DeadlineExceeded {
-		log.Info().Str("Username", requestCredentials.Username).Msg("Database query duration exceeded!")
+		slog.Info("Database query duration exceeded!", "Username", requestCredentials.Username)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Database Error!"))
 		return
 	}
 	if err == database.ErrOnFetchUserDoesNotExist {
-		log.Info().Str("Username", requestCredentials.Username).Msg("Login Request for Invalid Username")
+		slog.Info("Login Request for Invalid Username", "Username", requestCredentials.Username)
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("No user with given username exists!"))
 		return
 	}
 	if err != nil {
-		log.Error().Err(err).Msg("Found error during authentication!")
+		slog.Error("Found error during authentication!", "Error", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("An error occurred during authentication attempt, please try again."))
 		return
@@ -73,7 +73,7 @@ func (authMaster *AuthenticationMaster) Login(w http.ResponseWriter, r *http.Req
 
 	// Actually check if the user is who they say they are
 	if !ok {
-		log.Info().Str("Username", requestCredentials.Username).Msg("Invalid login attempt!")
+		slog.Info("Invalid login attempt!", "Username", requestCredentials.Username)
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Invalid Username or Password."))
 		return
@@ -84,7 +84,7 @@ func (authMaster *AuthenticationMaster) Login(w http.ResponseWriter, r *http.Req
 	userID, _ := authMaster.databaseConnection.GetUserIDByUsername(context.Background(), requestCredentials.Username)
 	token, err := authMaster.generateJWT(userID)
 	if err != nil {
-		log.Error().Err(err).Str("Username", requestCredentials.Username).Msg("Error during creation of JWT!")
+		slog.Error("Error during creation of JWT!", "Error", err, "Username", requestCredentials.Username)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("An error occurred during authentication attempt, please try again"))
 		return
